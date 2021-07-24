420b79f3ad7aa265287fdd0e2fe2898d800ce195
Richad MERALLI <richad.meralli@gmail.com>
2021-07-24 12:45:57 +0200

    added freelances cards

--- src/pages/Freelance/index.jsx
+++ src/pages/Freelance/index.jsx
@@ -1,5 +1,38 @@
-function Freelance() {
-  return <div>Ceci est la page Freelance</div>
+import DefaultPicture from '../../assets/profile.png'
+import Card from '../../components/Card/index'
+
+const freelanceProfiles = [
+  {
+    name: 'Jane Doe',
+    jobTitle: 'Devops',
+    picture: DefaultPicture,
+  },
+  {
+    name: 'John Doe',
+    jobTitle: 'Developpeur frontend',
+    picture: DefaultPicture,
+  },
+  {
+    name: 'Jeanne Biche',
+    jobTitle: 'Développeuse Fullstack',
+    picture: DefaultPicture,
+  },
+]
+
+function Freelances() {
+  return (
+    <div>
+      <h1>Freelances 👩‍💻👨‍💻👩‍💻</h1>
+      {freelanceProfiles.map((profile, index) => (
+        <Card
+          key={`${profile.name}-${index}`}
+          label={profile.jobTitle}
+          picture={profile.picture}
+          title={profile.name}
+        />
+      ))}
+    </div>
+  )
 }
 
-export default Freelance
+export default Freelances
