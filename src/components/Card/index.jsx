5a716a8bc30a60de9189643ab45d1e7edf699553
Richad MERALLI <richad.meralli@gmail.com>
2021-07-24 12:59:29 +0200

    added styled-components and prop-types

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,3 +1,6 @@
+import PropTypes from 'prop-types'
+import DefaultPicture from '../../assets/profile.png'
+
 function Card({ label, title, picture }) {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', padding: 15 }}>
@@ -8,4 +11,15 @@ function Card({ label, title, picture }) {
   )
 }
 
+Card.propTypes = {
+  label: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+}
+
+Card.defaultProps = {
+  title: 'Titre par défaut',
+  label: 'Label par défaut',
+  picture: DefaultPicture,
+}
 export default Card
