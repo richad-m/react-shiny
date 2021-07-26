1eb2e2d3cea0e79ce589cf26369633136825d984
Richad MERALLI <richad.meralli@gmail.com>
2021-07-26 10:28:03 +0200

    ok

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,12 +1,27 @@
 import PropTypes from 'prop-types'
 import DefaultPicture from '../../assets/profile.png'
+import styled from 'styled-components'
+
+//Styled components definitions
+
+const CardLabel = styled.span`
+  color: #5843e4;
+  font-size: 22px;
+  font-weight: bold;
+`
+const CardImage = styled.img`
+  height: 80px;
+  width: 80px;
+  border-radius: 50%;
+`
+const CardTitle = styled.span``
 
 function Card({ label, title, picture }) {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', padding: 15 }}>
-      <span>{label}</span>
-      <img src={picture} alt="freelance" height={80} width={80} />
-      <span>{title}</span>
+      <CardLabel>{label}</CardLabel>
+      <CardImage src={picture} alt="freelance" />
+      <CardTitle>{title}</CardTitle>
     </div>
   )
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,19 @@
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const LinkStyle = styled(Link)`
+  padding: 15px;
+  color: #8186a0;
+  text-decoration: none;
+  font-size: 18px;
+`
 
 function Header() {
   return (
     <nav>
-      <Link to="/"> Accueil </Link>
-      <Link to="/survey/42"> Questionnaire </Link>
-      <Link to="/freelance"> Freelance </Link>
+      <LinkStyle to="/"> Accueil </LinkStyle>
+      <LinkStyle to="/survey/42"> Questionnaire </LinkStyle>
+      <LinkStyle to="/freelance"> Freelance </LinkStyle>
     </nav>
   )
 }
