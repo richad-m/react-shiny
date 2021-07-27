81a25d7065deccc01c6458cd164d0d4b8f4a0275
Richad MERALLI <richad.meralli@gmail.com>
2021-07-27 16:06:55 +0200

    banner OK

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,72 @@
+import resume from '../assets/resume.svg'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import colors from '../utils/style/color'
+
+const ResumeSVG = styled.img`
+  height: 506px;
+  width: 541px;
+  left: 760px;
+  top: 339px;
+  border-radius: 0px;
+`
+const TestButton = styled(Link)`
+  padding: 15px;
+  color: #8186a0;
+  text-decoration: none;
+  font-size: 18px;
+  height: 40px;
+  width: 261px;
+  ${(props) =>
+    props.$isFullLink &&
+    `color: white; border-radius: 30px; background-color: ${colors.primary};`}
+`
+
+const CatchPhrase = styled.div`
+  height: 249px;
+  width: 552px;
+  left: 163px;
+  top: 376px;
+  border-radius: nullpx;
+  font-family: Trebuchet MS;
+  font-size: 50px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 80px;
+  letter-spacing: 0em;
+  text-align: left;
+`
+
+const Banner = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 3px;
+  height: 824px;
+  width: 1313px;
+  left: 65px;
+  top: 200px;
+  border-radius: 0px;
+`
+const ButtonWrapper = styled.div`
+  height: 40px;
+  width: 261px;
+`
 function Home() {
-  return <div>Page d'accueil 🏡</div>
+  return (
+    <Banner>
+      <CatchPhrase>
+        <div>
+          Repérez vos besoins, on s'occupe du reste, avec les meilleurs talents
+        </div>
+        <ButtonWrapper>
+          <TestButton to="/survey/1" $isFullLink>
+            Faire le test
+          </TestButton>
+        </ButtonWrapper>
+      </CatchPhrase>
+      <ResumeSVG src={resume} alt="shiny resume" />
+    </Banner>
+  )
 }
 
 export default Home
