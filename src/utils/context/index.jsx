e1ae07f7bf1f4dffa5e55ccc143e9a85095a1d47
Richad MERALLI <richad.meralli@gmail.com>
2021-07-28 19:01:24 +0200

    small fixes

--- src/utils/context/index.jsx
+++ src/utils/context/index.jsx
@@ -1,39 +1,22 @@
 import React, { useState, createContext } from 'react'
 
+// Creating context Theme related
 export const ThemeContext = createContext()
 
+// Creating Provider component with infos (state & fct)
+// Setting 'light' as default theme & creating a toggle function (light <-> dark)
 export const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState('light')
   const toggleTheme = () => {
     setTheme(theme === 'light' ? 'dark' : 'light')
+    console.log(theme)
   }
 
   return (
+    // Values are the info from the context which could be retrived by consumers
+    // Passing children to the components as we have no idea what would be passed to it
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
       {children}
     </ThemeContext.Provider>
   )
 }
-
-// import React, { useState, createContext } from 'react'
-
-// // Creating context Theme related
-// export const ThemeContext = createContext()
-
-// // Creating Provider component with infos (state & fct)
-// // Setting 'light' as default theme & creating a toggle function (light <-> dark)
-// export const ThemeProvider = ({ children }) => {
-//   const [theme, setTheme] = useState('light')
-//   const toggleTheme = () => {
-//     setTheme(theme === 'light' ? 'dark' : 'light')
-//     console.log(theme)
-//   }
-
-//   return (
-//     // Values are the info from the context which could be retrived by consumers
-//     // Passing children to the components as we have no idea what would be passed to it
-//     <ThemeContext.Provider value={{ theme, toggleTheme }}>
-//       {children}
-//     </ThemeContext.Provider>
-//   )
-// }
