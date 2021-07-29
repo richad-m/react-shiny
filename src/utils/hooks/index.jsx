60909a02296eb050bd2680b3fc1fff10c0d3a5fb
Richad MERALLI <richad.meralli@gmail.com>
2021-07-29 19:23:22 +0200

    added a hook for theme

--- src/utils/hooks/index.jsx
+++ src/utils/hooks/index.jsx
@@ -1,27 +1,25 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useContext } from 'react'
+import { ThemeContext } from '../context'
 
 export function useFetch(url) {
   const [data, setData] = useState({})
-
   const [isLoading, setLoading] = useState(true)
 
   useEffect(() => {
     if (!url) return
-
     async function fetchData() {
       const response = await fetch(url)
-
       const data = await response.json()
-
       setData(data)
-
       setLoading(false)
     }
-
     setLoading(true)
-
     fetchData()
   }, [url])
-
   return { isLoading, data }
+}
+
+export function useTheme() {
+  const { theme } = useContext(ThemeContext)
+  return { theme }
 }
