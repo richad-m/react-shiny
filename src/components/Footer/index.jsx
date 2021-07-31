b19b218c4fd5973984c97459210e90ab4c78ef88
Richad MERALLI <richad.meralli@gmail.com>
2021-07-31 11:33:23 +0200

    added input for email in the footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 import colors from '../../utils/style/color'
 import { useContext } from 'react'
 import { ThemeContext } from '../../utils/context'
+import { useState } from 'react'
 
 const FooterContainer = styled.footer`
   display: flex;
@@ -18,6 +19,20 @@ const NightModeButton = styled.button`
   color: ${colors.secondary};
 `
 
+function EmailInput() {
+  const [input, setInput] = useState('')
+  return (
+    <div>
+      <input
+        onChange={(e) => setInput(e.target.value)}
+        type="text"
+        placeholder="Entrez email"
+      />
+      <button type="submit">Entrer</button>
+    </div>
+  )
+}
+
 function Footer() {
   const { theme, toggleTheme } = useContext(ThemeContext)
 
@@ -26,6 +41,7 @@ function Footer() {
       <NightModeButton onClick={() => toggleTheme()}>
         Changer de mode {theme === 'light' ? '☀️' : '🌙'}
       </NightModeButton>
+      <EmailInput></EmailInput>
     </FooterContainer>
   )
 }
